64db01fc298d10f9c484ae9f3e62589d3d19829e
ReeChard <ebukammaduekwepaul@outlook.com>
2023-03-30 17:10:26 +0100

    Binary trees

--- 112-array_to_bst.c
+++ 112-array_to_bst.c
@@ -1,34 +1,33 @@
 #include "binary_trees.h"
 
 /**
- * array_to_bst - Builds a Binary Search Tree from an array.
- *
- * @array: A pointer to the first element of the array to be converted.
- * @size: The number of element in the array
- *
- * Return: A pointer to the root node of the created BST, or NULL on failure.
- */
+* array_to_bst - Builds a binary search tree from an array.
+* @array: A pointer to the first element of the array to be converted.
+* @size: The number of elements in @array.
+*
+* Return: A pointer to the root node of the created BST, or NULL upon failure.
+*/
 bst_t *array_to_bst(int *array, size_t size)
 {
-	bst_t *tree = NULL;
-	size_t i, j;
+bst_t *tree = NULL;
+size_t i, j;
 
-	if (array == NULL)
-		return (NULL);
+if (array == NULL)
+return (NULL);
 
-	for (i = 0; i < size; i++)
-	{
-		for (j = 0; j < index; j++)
-		{
-			if (array[j] == array[i])
-				break;
-		}
-		if (j == i)
-		{
-			if (bst_insert(&tree, array[i]) == NULL)
-				return (NULL);
-		}
-	}
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < i; j++)
+{
+if (array[j] == array[i])
+break;
+}
+if (j == i)
+{
+if (bst_insert(&tree, array[i]) == NULL)
+return (NULL);
+}
+}
 
-	return (tree);
+return (tree);
 }
